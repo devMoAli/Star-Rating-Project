82d9cdf1d19bf668ef91c2e9d5f5ef361e6bd3aa
Mohamed Ali <dev.mohamed.ali1@gmail.com>
2024-05-29 20:28:16 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import StarRating from "./components";
+
+function App() {
+  return (
+    <div className="App">
+      <StarRating noOfStars={5}/>
+    </div>
+  );
+}
+
+export default App;
